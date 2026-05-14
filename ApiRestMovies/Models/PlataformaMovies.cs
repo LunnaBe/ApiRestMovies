89c3ab1181e4285aefaeb2e2e73ad548110a6c00
Luna <luna.beatrizalves@gmail.com>
2026-05-13 21:51:07 -0300

    Implementação de classe PlataformaMovies - Luna

---
+++ ApiRestMovies/Models/PlataformaMovies.cs
@@ -0,0 +1,19 @@
+﻿namespace ApiRestMovies.Models
+{
+    public class PlataformaMovies
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Films { get; set; }
+
+        public string ShortFilms { get; set; }
+
+        public string TvShows { get; set; }
+
+        public string Url { get; set; }
+
+        public string ImageUrl { get; set;
+    }
+}
