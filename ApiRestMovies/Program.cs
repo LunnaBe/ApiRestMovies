6c4426fa96dfc141decd4c37e7de1a3ea5357f63
Luna <luna.beatrizalves@gmail.com>
2026-05-16 21:18:06 -0300

    Corrigindo caminho web - Luna

--- ApiRestMovies/Program.cs
+++ ApiRestMovies/Program.cs
@@ -6,6 +6,7 @@ using Google.Api.Gax.Grpc.Gcp;
 using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Firestore;
 using Google.Protobuf.WellKnownTypes;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,14 +35,9 @@ builder.Services.AddSwaggerGen(options =>
     options.IncludeXmlComments(xmlPath);
 });
 
-// Configuração de injeção de dependência para os serviços e repositórios, permitindo que as dependências
-// sejam resolvidas automaticamente pelo contêiner de injeção de dependência do ASP.NET Core.
-builder.Services.AddSingleton<PlataformaMovies>();
-builder.Services.AddScoped<MoviesService>();
-
 // Configuração das credencias do Firebase
 var caminhoCredencialFirabase = Path.Combine(Directory.GetCurrentDirectory(), "config_API/firebase-key.json");
-var credencial = GoogleCredential.FromFile(caminhoCredencialFirabase);
+var credential = GoogleCredential.FromFile(caminhoCredencialFirabase);
 
 // Configuração do FirestoreDbBuilder para criar uma instância do FirestoreDb, utilizando as
 // configurações definidas no arquivo de configuração da aplicação (appsettings.json).
@@ -50,8 +46,15 @@ var firestoreDb = new FirestoreDbBuilder
     // As configurações de ProjectId e CredentialsPath são lidas do arquivo de configuração da aplicação (appsettings.json) para garantir que
     // as credenciais e o ID do projeto sejam configurados corretamente.
     ProjectId = builder.Configuration["ApiConfig:ProjectId"],
-    CredentialsPath = builder.Configuration["ApiConfig:CredentialsPath"]
+    Credential = credential
 }.Build();
+
+// Adiciona o FirestoreDb como um serviço singleton no contêiner de injeção de dependência, garantindo que a mesma instância seja utilizada em toda a aplicação.
+builder.Services.AddSingleton(firestoreDb);
+
+// Configuração de injeção de dependência para os serviços e repositórios, permitindo que as dependências
+builder.Services.AddSingleton<PlataformaMovies>();
+builder.Services.AddScoped<MoviesService>();
 
 // Configuração de injeção de dependência para o FirestoreDb, permitindo que ele seja utilizado pelos repositórios e serviços que precisam acessar o banco de dados.
 builder.Services.AddCors(options =>
@@ -80,11 +83,8 @@ app.UseSwaggerUI(options =>
 // Configuração do middleware de CORS para permitir solicitações de qualquer origem, o que é útil durante o desenvolvimento e testes,
 // mas deve ser configurado com mais restrição em ambientes de produção para garantir a segurança da aplicação.
 app.UseCors("PermitirTudo");
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
